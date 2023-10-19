d5811fd8a56741287bc4d679fe3f1dfc82960527
Dunvant_kai <77610897+Dunvantkai@users.noreply.github.com>
2023-10-20 01:44:45 +1300

    Text_encrypt A-0.1.0
    Text_encrypt A-0.1.0

---
+++ Text_encrypt.java
@@ -0,0 +1,29 @@
+public class Text_encrypt{
+    public static void main(String[] args) {
+        String userInput = "cant even right now";
+        String UperUserInput = userInput.toUpperCase();
+        int lenthOfInput = UperUserInput.length();
+        int seed = 7;
+        int place = 0;
+        String encry = (">");
+        String auf = "ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
+
+        while (place < lenthOfInput) {
+            char letter = UperUserInput.charAt(place);
+
+            int NumberinAuf = auf.indexOf(letter);
+            if (NumberinAuf != -1) {
+                int AfterNumberinAuf = (NumberinAuf + seed) % auf.length();
+                char encryLetter = auf.charAt(AfterNumberinAuf);
+                String lencry = encry + encryLetter;
+                encry = lencry;
+                //System.out.println(encry);
+            }
+
+            place = place + 1;
+        }
+        System.out.println(UperUserInput);
+        System.out.println(encry);
+        }        
+}
+ 
