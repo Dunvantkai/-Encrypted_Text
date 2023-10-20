cf195a2ffa17d584bba079bc03c7ff3e4cffbddd
Dunvant_kai <77610897+Dunvantkai@users.noreply.github.com>
2023-10-20 16:56:11 +1300

    Added user inout
    Added user inout and changed seed to 7 not 3

--- Text_unencrypt.java
+++ Text_unencrypt.java
@@ -1,9 +1,13 @@
+import java.util.Scanner;
+
 public class Text_unencrypt {
     public static void main(String[] args) {
-        String userInput = "rhp";
+        Scanner myObj = new Scanner(System.in);
+        System.out.print("Enter Your Text: ");
+        String userInput = myObj.nextLine();
+        int seed = 9;
         String UperUserInput = userInput.toUpperCase();
         int lenthOfInput = UperUserInput.length();
-        int seed = 7;
         int place = 0;
         String encry = (">");
         String auf = "ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
